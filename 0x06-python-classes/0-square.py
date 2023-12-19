9cd3207ec271fdc038f623b7229a68d77b334895
azeez-abp <adioazeezadeyori@gmail.com>
2023-12-19 10:13:43 -0800

    make -x

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """square"""
-    def __init__(self):
-        """initialize square attributes"""
-        return None
+    """Empty class that defines a square"""
+    pass
