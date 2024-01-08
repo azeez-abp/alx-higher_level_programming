88f5f00ecb8cfc20851e37fb15dbef0f4af0bfd5
azeez-abp <adioazeezadeyori@gmail.com>
2024-01-07 23:17:59 -0800

    done test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,12 +5,18 @@ import unittest
 
 max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
-    def test_max(self):
-        """Test list is not empty"""
-        res = max_integer([1, 2, 3, 4])
-        self.assertEqual(res, 4)
+    """ class for unittests """
+    def test_max_integer(self):
+        """ check possible cases edge cases """
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([3]), 3)
+        self.assertEqual(max_integer([-3]), -3)
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+        self.assertEqual(max_integer([15, 10, 5]), 15)
+        self.assertEqual(max_integer([15, 10, 5, -5, -10, 15]), 15)
+        self.assertEqual(max_integer([15, 15, 15]), 15)
 
-    def test_empyt_list(self):
-        """Test empty list to return None"""
-        res  = max_integer([])
-        self.assertEqual(res, None)
+    def test_type_error(self):
+        """ type_errors """
+        self.assertRaises(TypeError, max_integer, ["h", 1])
+        self.assertRaises(TypeError, max_integer, [2, [2, 1]])
