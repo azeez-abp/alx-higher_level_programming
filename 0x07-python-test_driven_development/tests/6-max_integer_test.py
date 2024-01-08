c95faf602157077b9c62af8db582d7ffe27bc48b
azeez-abp <adioazeezadeyori@gmail.com>
2024-01-07 23:15:15 -0800

    Empty list test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,5 +6,11 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_max(self):
-        res =max_integer([1, 2, 3, 4])
+        """Test list is not empty"""
+        res = max_integer([1, 2, 3, 4])
         self.assertEqual(res, 4)
+
+    def test_empyt_list(self):
+        """Test empty list to return None"""
+        res  = max_integer([])
+        self.assertEqual(res, None)
