dec9ed1d0ad2a27685d7393d045eafdf41ad62d2
azeez-abp <adioazeezadeyori@gmail.com>
2024-03-17 11:02:35 -0700

    14 updated

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for city, state in session.query(City, State).\
-            filter(City.state_id == State.id).order_by(City.id).all():
+    for city, state in session.query(City, State)\
+            .filter(City.state_id == State.id).order_by(City.id).all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
