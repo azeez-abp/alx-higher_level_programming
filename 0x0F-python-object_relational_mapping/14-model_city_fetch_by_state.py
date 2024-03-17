dc88db6e1530865d5637e4e9ff475f36fcbfc207
root <root@dev.myguest.virtualbox.org>
2024-03-17 23:56:16 +0100

    14 with 3306 two2

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
+"""Start link class to table in database
 """
-Module for state data
-"""
-
 import sys
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
-from model_state import State
+from model_state import Base, State
 from model_city import City
-
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    session = Session(engine)
-
-    for city, state in session.query(City, State) \
-                              .filter(City.state_id == State.id) \
-                              .order_by(City.id):
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    State.cities = relationship("City",
+                                order_by=City.id, back_populates="state")
+    connection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    eng = create_engine(connection.format(user_name, password, db_name),
+                        pool_pre_ping=True)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    query = session.query(State, City).\
+        filter(City.state_id == State.id).all()
+    for row in query:
+        print("{}: ({}) {}".format(row[0].name, row[1].id, row[1].name))
